eccb177a0c2d98dae0d29fd00bc62df1cf9987c8
mjpritchard <matt.pritchard@stfc.ac.uk>
2020-06-19 13:44:44 +0100

    remove dep for now

--- setup.py
+++ setup.py
@@ -41,6 +41,6 @@ if __name__ == "__main__":
         packages = find_packages(),
         include_package_data = True,
         zip_safe = False,
-        install_requires = [ 'b3fwtheme-django', ],
+        install_requires = [ ],
         extras_require = { },
     )
